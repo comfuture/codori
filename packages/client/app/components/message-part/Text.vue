d0e89dee95d4c460daeece0dc1c91adf4b96810a
comfuture <comfuture@gmail.com>
2026-04-14 15:10:01 +0900

    Address markdown rendering review feedback

--- packages/client/app/components/message-part/Text.vue
+++ packages/client/app/components/message-part/Text.vue
@@ -3,7 +3,7 @@ import { Comark } from '@comark/vue'
 import highlight from '@comark/vue/plugins/highlight'
 import math, { Math as ComarkMath } from '@comark/vue/plugins/math'
 import mermaid, { Mermaid } from '@comark/vue/plugins/mermaid'
-import { computed } from 'vue'
+import { computed, defineComponent, h, type Component } from 'vue'
 
 const props = defineProps<{
   role?: 'user' | 'assistant' | 'system'
@@ -14,9 +14,49 @@ const props = defineProps<{
   } | null
 }>()
 
+const ResponsiveMermaid = Mermaid as Component
+
+const ChatMarkdownMermaid = defineComponent({
+  name: 'ChatMarkdownMermaid',
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    class: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: String,
+      default: ''
+    },
+    theme: {
+      type: [String, Object],
+      default: undefined
+    },
+    themeDark: {
+      type: [String, Object],
+      default: undefined
+    }
+  },
+  setup(props, { attrs }) {
+    return () => h(ResponsiveMermaid, {
+      ...attrs,
+      ...props,
+      height: props.height || 'auto',
+      width: props.width || '100%'
+    })
+  }
+})
+
 const components = {
   math: ComarkMath,
-  mermaid: Mermaid
+  mermaid: ChatMarkdownMermaid
 }
 
 const plugins = [
